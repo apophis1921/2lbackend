cdfcf549ec8ab3863acc73c21008a048ef33df04
apophis1921 <apophis1921@gmail.com>
2026-04-19 16:38:15 +0530

    github data added

--- index.js
+++ index.js
@@ -3,6 +3,46 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const gitdata={
+  "login": "apophis1921",
+  "id": 250696799,
+  "node_id": "U_kgDODvFUXw",
+  "avatar_url": "https://avatars.githubusercontent.com/u/250696799?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/apophis1921",
+  "html_url": "https://github.com/apophis1921",
+  "followers_url": "https://api.github.com/users/apophis1921/followers",
+  "following_url": "https://api.github.com/users/apophis1921/following{/other_user}",
+  "gists_url": "https://api.github.com/users/apophis1921/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/apophis1921/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/apophis1921/subscriptions",
+  "organizations_url": "https://api.github.com/users/apophis1921/orgs",
+  "repos_url": "https://api.github.com/users/apophis1921/repos",
+  "events_url": "https://api.github.com/users/apophis1921/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/apophis1921/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": null,
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 1,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 0,
+  "created_at": "2025-12-20T12:59:58Z",
+  "updated_at": "2025-12-20T12:59:58Z"
+}
+
+app.get('/github',(req,res)=>{
+  res.json(gitdata)
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
